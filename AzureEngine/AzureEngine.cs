f11066878960bc75dbf8ca646ecf01d9f99635f5
Fraser Greenroyd <fraser.greenroyd@burohappold.com>
2018-06-18 12:46:09 +0100

    Connecting to a storage account using our access keys

--- AzureEngine/AzureEngine.cs
+++ AzureEngine/AzureEngine.cs
@@ -4,9 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
 namespace AzureEngine
 {
     public class AzureEngine
     {
+        private CloudStorageAccount storageAccount = null;
+
+        public void InitStorage(String connectionString = null)
+        {
+            if (connectionString == null)
+                connectionString = Environment.GetEnvironmentVariable("azureStorageConnectionString");
+
+            if(CloudStorageAccount.TryParse(connectionString, out storageAccount))
+            {
+                Console.WriteLine("Successfully connected to storage account");
+            }
+            else
+            {
+                //Failure - alert user in a friendly manner
+                Console.WriteLine("An error occurred in connecting to the storage account");
+                Console.WriteLine("Please ensure the connection string is correct.");
+                Console.WriteLine("Attempted to use connection string: " + connectionString);
+            }
+        }
     }
 }
